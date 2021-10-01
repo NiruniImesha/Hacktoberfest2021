492559718ccbcb417ef84c9cdf0bf01a2d93a813
Niruni Imesha <88593456+NiruniImesha@users.noreply.github.com>
2021-10-02 00:44:47 +0530

    Update perimeter of the rectangle.cpp

--- perimeter of the rectangle.cpp
+++ perimeter of the rectangle.cpp
@@ -3,12 +3,12 @@
     int main()
     {
     	int width, lngth, peri;	
-        cin<<" Input the length of the rectangle : ";
-    	cout>>lngth;
+        cout<<" Input the length of the rectangle : ";
+    	cin>>lngth;
 		  cin<<" Input the width of the rectangle : ";
-    	cout>>width;
+    	cin>>width;
 		  peri=2*(lngth+width);
-        cin<<" The perimeter of the rectangle is : "<< peri << endl;		
-        cin << endl;
+        cout<<" The perimeter of the rectangle is : "<< peri << endl;		
+        cout<< endl;
         return 0;
     }
